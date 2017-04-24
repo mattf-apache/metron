14a58f44750d33c7e4fd95de0f76a3717a37fb9c
cstella <cestella@gmail.com>
2017-04-24 16:02:17 -0400

    Whoops, missed one.

--- metron-platform/metron-data-management/src/main/java/org/apache/metron/dataloads/nonbulk/flatfile/LoadOptions.java
+++ metron-platform/metron-data-management/src/main/java/org/apache/metron/dataloads/nonbulk/flatfile/LoadOptions.java
@@ -21,9 +21,9 @@ import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
 import org.apache.commons.cli.*;
 import org.apache.commons.io.FileUtils;
-import org.apache.metron.common.utils.ConversionUtils;
 import org.apache.metron.common.utils.cli.OptionHandler;
 import org.apache.metron.dataloads.nonbulk.flatfile.importer.ImportStrategy;
+import org.apache.metron.stellar.common.utils.ConversionUtils;
 
 import javax.annotation.Nullable;
 import java.io.File;
